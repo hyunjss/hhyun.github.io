298264cc1135338de8562b5173b8fd331b99341a
hyunjss <dkgus66@gmail.com>
2026-04-28 13:19:59 +0900

    fix: base path 수정

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // username.github.io 형태라 '/' 사용
+  base: '/hhyun.github.io/',
 })
